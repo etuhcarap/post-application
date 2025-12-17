1cd5ca662879c3e1fcbb0e272d64c3aee942db4f
etuhcarap <etuhcarap.log@gmail.com>
2025-12-17 12:13:15 +0900

    feat: add post domain model and jpa-auditing config

---
+++ src/main/java/io/sparta/postapplication/domains/post/domain/model/Post.java
@@ -0,0 +1,43 @@
+package io.sparta.postapplication.domains.post.domain.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Post {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
+	private Long id;
+
+	@Column
+	private String title;
+
+	@Column
+	private String content;
+
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	@Getter
+	LocalDateTime createdAt;
+
+	public Post(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+}
---
+++ src/main/java/io/sparta/postapplication/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package io.sparta.postapplication.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
